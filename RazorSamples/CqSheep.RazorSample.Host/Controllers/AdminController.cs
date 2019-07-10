a5b42593dc290ef60e63535e94a9a691cc1ec9fa
xeekseven <xeekseven@outlook.com>
2019-07-11 00:07:36 +0800

    初始界面完成

--- RazorSamples/CqSheep.RazorSample.Host/Controllers/AdminController.cs
+++ RazorSamples/CqSheep.RazorSample.Host/Controllers/AdminController.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CqSheep.RazorSample.Host.Controllers
-{
-    public class AdminController : Controller
-    {
+namespace CqSheep.RazorSample.Host.Controllers {
+    public class AdminController : Controller {
         [HttpGet]
-        public IActionResult Auth()
-        {
-            return View();
+        public IActionResult Auth () {
+            return View ();
+        }
+
+        [HttpGet]
+        public IActionResult Overview () {
+            return View ();
         }
     }
 }
